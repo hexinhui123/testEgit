905d707104cf2773fa20ee5f6df98b1d23c71611
Xzl888 <438224630@qq.com>
2019-09-19 14:12:02 +0800

    zz

--- src/com/atguigu/egit/test/EgitTest.java
+++ src/com/atguigu/egit/test/EgitTest.java
@@ -5,6 +5,10 @@ public class EgitTest {
 		System.out.println("aaaa");
 		System.out.println("bbbb");
 		System.out.println("cccc");
+		System.out.println("嘻嘻嘻");
+		System.out.println("嘿嘿嘿");
+		System.out.println("略略略~~~");
+		
 	}
 
 }
